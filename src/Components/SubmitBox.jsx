addf004b6b396ff217df61ba8b8ef7481fd98f20
coderseyfi <seyfaddinmn@code.edu.az>
2023-10-03 20:14:04 +0400

    cmplete

--- src/Components/SubmitBox.jsx
+++ src/Components/SubmitBox.jsx
@@ -7,7 +7,7 @@ const SubmitBox = () => {
     <div className="form-submit-message">
       <div className="submit-box">
         <img src={Submit} alt="" />
-        <p className="success">Uğurlu göndərildi</p>
+        <p className="success">Müraciətiniz uğurla göndərildi</p>
       </div>
     </div>
   )
